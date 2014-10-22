a31fafc12806db9ab063907e7fd1eb39343941ea
cris1403 <c.mastrofrancesco@gmail.com>
2014-10-22 21:07:00 +0200

    Initial code

--- EDA-project.R
+++ EDA-project.R
@@ -17,8 +17,22 @@ rm(td, tf)                                # remove unnecessary stuff
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
+NEI$fips = factor(NEI$fips)
+NEI$SCC = factor(NEI$SCC)
+NEI$Pollutant = factor(NEI$Pollutant)
+NEI$year = factor(NEI$year)
+
 
 # Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
 # Using the base plotting system, make a plot showing the total PM2.5 emission from all sources 
 # for each of the years 1999, 2002, 2005, and 2008.
-library(dplyr)
+library(plyr)
+dataset = as.data.frame(ddply(NEI, ~yea, summarise, tot=sum(Emissions)))
+
+plot(dataset$year, dataset$tot, type="p", col="red")
+
+plot_1 <- aggregate(Emissions ~ year, NEI, sum)
+plot(plot_1$year,plot_1$Emissions, main="Total US PM2.5 Emissions", "b", xlab="Year", ylab="Emissions (thousands of tons)",xaxt="n")
+axis(side=1, at=c("1999", "2002", "2005", "2008"))
+plot(dataset$year, dataset$tot, main="Total US PM2.5 Emissions", type="b", xlab="Year", ylab="Emissions (thousands of tons)",xaxt="n")
+axis(side=1, at=c("1999", "2002", "2005", "2008"))
